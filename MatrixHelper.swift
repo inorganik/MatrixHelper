d92568f4105155009ab358fb4119f1462d98d10a
Jamie Perkins <jamie.perkins@gmail.com>
2017-09-15 12:13:53 -0600

    initial commit of MatrixHelper

---
+++ MatrixHelper.swift
@@ -0,0 +1,50 @@
+//
+//  MatrixHelper.swift
+//  Ghost Selfie
+//
+//  Created by Jamie Perkins on 9/14/17.
+//  Copyright © 2017 Inorganik Produce, Inc. All rights reserved.
+//
+
+import Foundation
+import GLKit
+
+struct MatrixHelper {
+    
+    static func radiansToDegrees(_ radians: Float) -> Float {
+        return radians * (180.0 / Float.pi)
+    }
+    static func degreesToRadians(_ degrees: Float) -> Float {
+        return degrees * (Float.pi / 180.0)
+    }
+    
+    // GLKMatrix to float4x4
+    static func convertGLKMatrix4Tosimd_float4x4(_ matrix: GLKMatrix4) -> float4x4{
+        return float4x4(float4(matrix.m00, matrix.m01, matrix.m02, matrix.m03),
+                        float4(matrix.m10, matrix.m11, matrix.m12, matrix.m13),
+                        float4(matrix.m20, matrix.m21, matrix.m22, matrix.m23),
+                        float4(matrix.m30, matrix.m31, matrix.m32, matrix.m33))
+    }
+	
+    // degrees - 0: straight ahead. positive: to the left. negative: to the right
+    static func rotateMatrixAroundY(degrees: Float, matrix: simd_float4x4) -> simd_float4x4 {
+        let radians = degreesToRadians(degrees)
+        let rotationMatrix = GLKMatrix4MakeYRotation(radians)
+        return simd_mul(convertGLKMatrix4Tosimd_float4x4(rotationMatrix), matrix)
+    }
+    
+    // degrees - 0: horizon. positive: toward sky. negative: toward ground
+    static func translateMatrixFromHorizon(degrees: Float, matrix: simd_float4x4) -> simd_float4x4 {
+
+        let radians = self.degreesToRadians(degrees)
+        let horizonMatrix = GLKMatrix4MakeXRotation(radians)
+        return simd_mul(convertGLKMatrix4Tosimd_float4x4(horizonMatrix), matrix)
+    }
+    
+    // just what it says on the tin
+    static func resetToHorizon(_ matrix: simd_float4x4) -> simd_float4x4 {
+        var resultMatrix = matrix
+        resultMatrix.columns.3.y = 0
+        return resultMatrix
+    }
+}
